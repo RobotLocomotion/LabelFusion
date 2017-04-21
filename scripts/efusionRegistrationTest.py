c638c939bfdaedf6713a2d57a8bdb9fc7d8872be
Pat Marion <james.patrick.marion@gmail.com>
2017-04-21 13:05:04 -0400

    add efusionRegistrationTest.py (nothing here yet)

---
+++ scripts/efusionRegistrationTest.py
@@ -0,0 +1,29 @@
+'''
+
+# phone
+pick_point 0.75556, -0.04087, -0.23920
+pick_normal -0.79761, 0.29078, 0.52846
+dist_to_previous_point 0.000000
+
+# toothpaste
+pick_point 0.66325, -0.12406, -0.11679
+pick_normal -0.46777, -0.05299, 0.88226
+dist_to_previous_point 0.174430
+
+# oil bottle
+pick_point 0.78493, -0.03217, -0.01731
+pick_normal -0.63093, -0.11345, 0.76750
+dist_to_previous_point 0.182061
+
+# toy robot
+pick_point 0.76878, 0.10297, -0.14594
+pick_normal -0.69869, -0.01452, 0.71528
+dist_to_previous_point 0.187268
+
+# table
+pick_point 0.78427, 0.03044, -0.24667
+pick_normal -0.76159, 0.16045, 0.62788
+dist_to_previous_point 0.125088
+
+
+'''

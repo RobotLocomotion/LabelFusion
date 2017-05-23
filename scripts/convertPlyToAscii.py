19bcae505756f1b8cc3720beb76ad6e3f09ad2a9
peteflorence <pete.florence@gmail.com>
2017-05-23 14:55:25 -0400

    ply to ascii conversion script which will clone separate git repo upon first time use

---
+++ scripts/convertPlyToAscii.py
@@ -0,0 +1,11 @@
+import os
+
+if not (os.path.isdir(os.getcwd() + "/../src/ply")):
+	os.system("cd .. && mkdir src && cd src && git clone https://github.com/peteflorence/ply.git")	
+	os.system("cd ../src/ply && make")	
+
+path = os.getcwd() + "/../data/logs/pipeline-test/"
+filename = "trimmed-log.lcmlog.ply"
+#print path
+
+os.system("../src/ply/ply2ascii <" + path + filename + "> " + path + "converted.ply")

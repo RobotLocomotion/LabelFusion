a475ad2c44b2c748cec2a0b962fa0a3f2de0e6cc
peteflorence <pete.florence@gmail.com>
2017-05-24 13:49:11 -0400

    hack the ply header to be accepted into director

--- scripts/prepareForObjectAlignment.py
+++ scripts/prepareForObjectAlignment.py
@@ -5,6 +5,7 @@ import yaml
 # Run this script from the log folder from which you want to process data
 
 # ------------------------------
+path_to_spartan = "/home/peteflo/spartan"
 
 
 ######################
@@ -22,12 +23,39 @@ f = open(yaml_path)
 dataMap = yaml.safe_load(f)
 lcmlog_filename = dataMap["lcmlog"]
 
-os.system(path_to_ElasticFusion_executable + " -l ./" + lcmlog_filename + " -f")
+#os.system(path_to_ElasticFusion_executable + " -l ./" + lcmlog_filename + " -f")
+
+# TODO: give error if multiple posegraph files
+os.system("mv " + lcmlog_filename + ".posegraph posegraph.posegraph")
 
 
 ###################################################
 # Convert from binary to ascii .ply, then to .vtp #
 ###################################################
 
+# install ply if do not already have it
+path_to_ply = path_to_spartan + "/src/CorlDev/src/ply"
+if not (os.path.isdir(path_to_ply)):
+ 	os.system("cd " + path_to_ply + "../ && mkdir src && cd src && git clone https://github.com/peteflorence/ply.git")	
+ 	os.system("cd " + path_to_ply + " && make")	
 
-#os.system("../src/ply/ply2ascii <" + path + filename + "> " + path + "converted.ply")
+ply_binary_filename = lcmlog_filename + ".ply"
+
+os.system(path_to_ply + "/ply2ascii <./" + ply_binary_filename + "> ./converted_to_ascii.ply")
+
+with open("./converted_to_ascii_modified_header.ply", 'w') as outfile:
+    with open(path_to_spartan + "/src/CorlDev/scripts/correct_ply_header.txt") as infile:
+        for line in infile:
+            outfile.write(line)
+    with open("./converted_to_ascii.ply") as infile:
+    	num_skip = 14
+    	counter = 0
+    	for line in infile:
+    		counter += 1
+    		if counter <= 14:
+    			continue
+    		outfile.write(line)
+
+
+print "directorPython " + path_to_spartan + "/src/CorlDev/scripts/convertPlyToVtp.py " +  "/home/peteflo/spartan/src/CorlDev/data/logs/pipeline-test/converted_to_ascii.ply"
+os.system("directorPython " + path_to_spartan + "/src/CorlDev/scripts/convertPlyToVtp.py " +  "/home/peteflo/spartan/src/CorlDev/data/logs/pipeline-test/converted_to_ascii_modified_header.ply")

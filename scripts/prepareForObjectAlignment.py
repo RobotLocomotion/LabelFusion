5a7ea2028fb4fadad43f7b7c54e53395686459ff
peteflorence <pete.florence@gmail.com>
2017-05-24 14:21:41 -0400

    some helpful comments, and quit out if no ElasticFusion

--- scripts/prepareForObjectAlignment.py
+++ scripts/prepareForObjectAlignment.py
@@ -15,6 +15,10 @@ path_to_spartan = os.environ['SPARTAN_SOURCE_DIR']
 # or at least check for it
 # for now, just need to specify ElasticFusion executable location
 path_to_ElasticFusion_executable = "/home/peteflo/ElasticFusion/GUI/build/ElasticFusion"
+if not (os.path.isfile(path_to_ElasticFusion_executable)):
+	print "You need to install ElasticFusion and change the path to the executable in scripts/prepareForObjectAlignment.py"
+	quit()
+
 
 path = os.getcwd()
 yaml_path = path + "/info.yaml"
@@ -22,8 +26,10 @@ f = open(yaml_path)
 dataMap = yaml.safe_load(f)
 lcmlog_filename = dataMap["lcmlog"]
 
+# call ElasticFusion
 os.system(path_to_ElasticFusion_executable + " -l ./" + lcmlog_filename + " -f")
 
+# rename posegraph
 # TODO: give error if multiple posegraph files
 os.system("mv " + lcmlog_filename + ".posegraph posegraph.posegraph")
 
@@ -40,8 +46,11 @@ if not (os.path.isdir(path_to_ply)):
 
 ply_binary_filename = lcmlog_filename + ".ply"
 
+# call ply2ascii
 os.system(path_to_ply + "/ply2ascii <./" + ply_binary_filename + "> ./converted_to_ascii.ply")
 
+# change header to be compatible with Director
+# TODO: make so Director accepts other header?
 with open("./converted_to_ascii_modified_header.ply", 'w') as outfile:
     with open(path_to_spartan + "/src/CorlDev/scripts/correct_ply_header.txt") as infile:
         for line in infile:
@@ -55,5 +64,9 @@ with open("./converted_to_ascii_modified_header.ply", 'w') as outfile:
     			continue
     		outfile.write(line)
 
+# convert to vtp
 os.system("directorPython " + path_to_spartan + "/src/CorlDev/scripts/convertPlyToVtp.py " +  "/home/peteflo/spartan/src/CorlDev/data/logs/pipeline-test/converted_to_ascii_modified_header.ply")
+
+# clean up and rename
+os.system("rm *.ply")
 os.system("mv converted_to_ascii_modified_header.vtp reconstructed_pointcloud.vtp")

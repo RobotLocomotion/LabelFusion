9c6c73244eb0a72a9e1072269444a5c46e73b751
peteflorence <pete.florence@gmail.com>
2017-06-07 22:57:02 -0400

    auto_alignment_tool !

---
+++ automation/scripts/autoAlignmentTool.py
@@ -0,0 +1,54 @@
+import os
+import yaml
+import csv
+import re
+import sys
+
+# Instructions:
+# Run this script from anywhere
+
+# ------------------------------
+path_to_spartan  = os.environ['SPARTAN_SOURCE_DIR']
+path_to_data     = path_to_spartan + "/src/CorlDev/data"
+
+# folders in /data to track
+folders = ["logs_test"]
+
+lcmlog_pattern = re.compile("lcmlog")
+
+progress_file_fullpath = ""
+
+path_to_job_folder = ""
+
+for folder in folders:
+    path_to_folder = path_to_data + "/" + folder 
+    progress_file_fullpath = os.path.join(path_to_folder, "auto_alignment_tool_in_progress.txt")
+
+    if os.path.isfile(progress_file_fullpath):               # check wip condition
+    	break
+
+    for subdir, dirs, files in os.walk(path_to_folder):
+        for dir in sorted(dirs):
+          
+            fullpath = os.path.join(subdir, dir)
+
+            if not os.path.isfile(os.path.join(fullpath,"reconstructed_pointcloud.vtp")):	 # check pre condition
+            	continue
+
+            if os.path.isfile(os.path.join(fullpath, "registration_results.yaml")):      # check post condition
+            	continue
+
+            path_to_job_folder = fullpath
+            break
+            
+        break # don't want recursive walk
+
+
+if path_to_job_folder == "":
+	quit()						# didn't find any jobs
+
+print "Found a job! in ", path_to_job_folder
+
+os.system("touch " + progress_file_fullpath)              # mark as wip
+os.system("cd " + path_to_job_folder + " && run_alignment_tool")
+os.system("rm " + progress_file_fullpath)                 # delete wip

cd2cbedf9aeb69557193e156caf4eea9365cde13
peteflorence <pete.florence@gmail.com>
2017-06-09 10:23:57 -0400

    scp magic

---
+++ automation/scp_out.py
@@ -0,0 +1,25 @@
+import os
+import yaml
+import sys
+
+source = sys.argv[1]
+destination_user = sys.argv[2]
+
+scp_config_file = "scp_config.yaml"
+with open(scp_config_file, 'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+for i in config:
+	if i == destination_user:
+		print "found", i, "credentials"
+		destination = config[i]['destination']
+		address     = config[i]['address']
+		port        = config[i]['port']
+		pw			= config[i]['pw']
+
+print "sending to ", destination_user
+os.system("sshpass -p '" + pw + "' scp -r -P " + port + " " + source + " " + address+":"+destination)
+print "sent", source
